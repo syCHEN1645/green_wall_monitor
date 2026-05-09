af7ab3bffdc72a7ff387b3a8b1b8eefb2075b5f3
syCHEN1645 <mreredast@gmail.com>
2026-05-09 19:04:20 +0800

    add and calibrate soil moisture sensor

--- esp_code/main/main.cpp
+++ esp_code/main/main.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <string.h>
+#include <string>
 #include <vector>
 #include <memory>
 #include "freertos/FreeRTOS.h"
@@ -14,11 +14,14 @@
 #include "SensorDevice.hpp"
 #include "DS18B20Device.hpp"
 #include "SEN0385Device.hpp"
+#include "SEN0308Device.cpp"
 #include "WiFiManager.hpp"
 #include "MqttPublisher.hpp"
 #include "config.h"
 
 // use smart pointers as placeholder to prevent startup order issues
+adc_oneshot_unit_handle_t adc_handle;
+
 std::unique_ptr<WiFiManager> wifiManager;
 std::unique_ptr<MqttPublisher> mqttPub;
 
@@ -29,34 +32,55 @@ void initialiseHardware() {
         ESP_LOGE("HardwareInit", "Error initializing I2C");
     }
     ESP_LOGI("HardwareInit", "Initialized I2C");
+
+    // initialize ADC unit
+    adc_oneshot_unit_init_cfg_t init_config = {
+        .unit_id = ADC_UNIT_1,
+        .clk_src = ADC_RTC_CLK_SRC_DEFAULT,
+        .ulp_mode = ADC_ULP_MODE_DISABLE,
+    };
+    esp_err_t err = adc_oneshot_new_unit(&init_config, &adc_handle);
+    if (err != ESP_OK) {
+        ESP_LOGE("HardwareInit", "Failed to initialize ADC unit %d: %s", ADC_UNIT_1, esp_err_to_name(err));
+    }
+    ESP_LOGI("HardwareInit", "Initialized ADC");   
 }
 
 void initialiseSensor() {
     // Use make_unique to manage memory automatically
-    auto sensor_1 = std::make_unique<SEN0385Device>("air_temp_1", CONFIG_SEN0385_I2C_ADDR);
-    int args_1[] = {CONFIG_SEN0385_SDA_PIN, CONFIG_SEN0385_SCL_PIN};
-    if (sensor_1->setupSensor(args_1) == ESP_OK) {
-        sensors.push_back(std::move(sensor_1));
-    } else {
-        ESP_LOGE("SensorInit", "Failed to initialize SEN0385 sensor");
-    }
+    // auto sensor_1 = std::make_unique<SEN0385Device>("air_temp_1", CONFIG_SEN0385_I2C_ADDR);
+    // int args_1[] = {CONFIG_SEN0385_SDA_PIN, CONFIG_SEN0385_SCL_PIN};
+    // if (sensor_1->setupSensor(args_1) == ESP_OK) {
+    //     sensors.push_back(std::move(sensor_1));
+    // } else {
+    //     ESP_LOGE("SensorInit", "Failed to initialize air_temp_1 sensor");
+    // }
 
-    auto sensor_2 = std::make_unique<DS18B20Device>("water_temp_1");
-    int args_2[] = {CONFIG_DS18B20_1_PIN};
-    if (sensor_2->setupSensor(args_2) == ESP_OK) {
-        sensors.push_back(std::move(sensor_2));
-    } else {
-        ESP_LOGE("SensorInit", "Failed to initialize DS18B20 sensor");
-    }
+    // auto sensor_2 = std::make_unique<DS18B20Device>("water_temp_1");
+    // int args_2[] = {CONFIG_DS18B20_1_PIN};
+    // if (sensor_2->setupSensor(args_2) == ESP_OK) {
+    //     sensors.push_back(std::move(sensor_2));
+    // } else {
+    //     ESP_LOGE("SensorInit", "Failed to initialize water_temp_1 sensor");
+    // }
 
-    auto sensor_3 = std::make_unique<DS18B20Device>("water_temp_2");
-    int args_3[] = {CONFIG_DS18B20_2_PIN};
-    if (sensor_3->setupSensor(args_3) == ESP_OK) {
-        sensors.push_back(std::move(sensor_3));
+    // auto sensor_3 = std::make_unique<DS18B20Device>("water_temp_2");
+    // int args_3[] = {CONFIG_DS18B20_2_PIN};
+    // if (sensor_3->setupSensor(args_3) == ESP_OK) {
+    //     sensors.push_back(std::move(sensor_3));
+    // } else {
+    //     ESP_LOGE("SensorInit", "Failed to initialize water_temp_2 sensor");
+    // }
+
+    auto sensor_4 = std::make_unique<SEN0308Device>("soil_moisture_1", adc_handle);
+    int args_4[] = {CONFIG_SEN0308_ADC_CHANNEL};
+    // sensor_4->setupSensor(args_4);
+    if (sensor_4->setupSensor(args_4) == ESP_OK) {
+        // after running this, sensor_4 points to null, avoid using it after this
+        sensors.push_back(std::move(sensor_4));
     } else {
-        ESP_LOGE("SensorInit", "Failed to initialize DS18B20 sensor");
+        ESP_LOGI("SensorInit", "Initialized sensor %s", sensor_4->name.c_str());
     }
-    
 }
 
 void initialiseWiFi() {
@@ -77,7 +101,6 @@ void sensor_task(void *pvParameters) {
             std::vector<float> readings = s->getReadingOnce();
             for (size_t i = 0; i < readings.size(); i++) {
                 printf("Reading %zu: %.2f\n" , i, readings[i]);
-                mqttPub->publish("topic", (std::to_string(readings[i])).c_str(), 0, 0);
             }
         }
         vTaskDelay(pdMS_TO_TICKS(DATA_PUBLISH_INTERVAL_FAST_S * 1000));
@@ -97,9 +120,9 @@ extern "C" void app_main(void) {
 
     // 2. Network / SNTP Initialization (Equivalent to your setup())
     // Note: You'll need a standard Wi-Fi helper here 
-    initialiseWiFi();
+    // initialiseWiFi();
     vTaskDelay(pdMS_TO_TICKS(1000)); // Wait a bit for Wi-Fi to stabilize
-    initialiseMqtt();
+    // initialiseMqtt();
     initialiseSensor();
     
     // 3. Set Timezone (Your UTC-8 logic)
